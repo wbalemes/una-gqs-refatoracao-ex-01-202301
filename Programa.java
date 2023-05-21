e9f3869765e2206473296df741cd0301d2a498aa
Warley Lemes <warleybruno54@hotmail.com>
2023-05-21 12:19:04 -0300

    fix: habilitado o método .close() para Scanner. Realizado indentação

--- Programa.java
+++ Programa.java
@@ -1,24 +1,24 @@
 import java.util.Scanner;
 
 class Programa {
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    boolean erroTentativas = false;
-    Pessoa pessoa = new Pessoa();
-    Mensagem mensagem = new Mensagem();
-    Validacao validacao = new Validacao();
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		boolean erroTentativas = false;
+		Pessoa pessoa = new Pessoa();
+		Mensagem mensagem = new Mensagem();
+		Validacao validacao = new Validacao();
 
-    System.out.println("\tRelatório Auxiliar de Controle de Infecções");
+		System.out.println("\tRelatório Auxiliar de Controle de Infecções");
 
-    pessoa.coletaNome(sc);
-    pessoa.coletaIdade(sc);
+		pessoa.coletaNome(sc);
+		pessoa.coletaIdade(sc);
 
-    erroTentativas = mensagem.realizaPerguntaVacinaEmDia(pesssoa);
+		erroTentativas = mensagem.realizaPerguntaVacinaEmDia(pesssoa);
 
-    validacao.validacaoDasProximasPerguntas(pessoas);
+		validacao.validacaoDasProximasPerguntas(pessoas);
 
-    validacao.calcularValidacaoFinal(erroTentativass);
+		validacao.calcularValidacaoFinal(erroTentativass);
 
-    // scanner.close();
-  }
+		scanner.close();
+	}
 }
